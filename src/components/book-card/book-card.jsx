884ceee100e3ed4c86db4f17389b8faca0208af2
toddmerlin <toddmerlin@outlook.com>
2024-02-15 15:03:10 +1100

    Create components

---
+++ src/components/book-card/book-card.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+
+export const BookCard = ({ book, onBookClick }) => {
+  return (
+    <div
+      onClick={() => {
+        onBookClick(book);
+      }}
+    >
+      {book.title}
+    </div>
+  );
+};
+
+BookCard.propTypes = {
+  book: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    author: PropTypes.string,
+  }).isRequired,
+  onBookClick: PropTypes.func.isRequired,
+};
---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+
+import { BookCard } from "../book-card/book-card";
+import { BookView } from "../book-view/book-view";
+
+export const MainView = () => {
+  const [books, setBooks] = useState([]);
+
+  const [selectedBook, setSelectedBook] = useState(null);
+
+  useEffect(() => {
+    fetch("https://openlibrary.org/search.json?q=star+wars")
+      .then((response) => response.json())
+      .then((data) => {
+        const booksfromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+            image: `https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`,
+            author: doc.author_name?.[0],
+          };
+        });
+
+        setBooks(booksfromApi);
+      });
+  }, []);
+
+  if (selectedBook) {
+    return (
+      <BookView book={selectedBook} onBackClick={() => setSelectedBook(null)} />
+    );
+  }
+
+  if (books.length === 0) {
+    return <div>The list is empty!</div>;
+  }
+
+  return (
+    <div>
+      {books.map((book) => (
+        <BookCard
+          key={book.id}
+          book={book}
+          onBookClick={(newSelectedBook) => {
+            setSelectedBook(newSelectedBook);
+          }}
+        />
+      ))}
+    </div>
+  );
+};
